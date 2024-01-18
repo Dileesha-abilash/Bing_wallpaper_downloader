989ac04cd2c6baf6b73b83aad04a169b2964580f
[dileesha] <[dileeshabilash@gmail.com]>
2024-01-18 19:53:56 +0530

    It is use to Download the images

---
+++ direct_down.py
@@ -0,0 +1,10 @@
+import requests
+ary = open("downs.txt","r").readlines()
+
+
+for i in range(len(ary)):
+    if i<=4:
+        print("downloading ",i)
+        r = requests.get(ary[i], allow_redirects=True)
+        g= str(i)+".jpg"
+        open(g, 'wb').write(r.content)
